d65ab85ffd127b19e9bb4b818e9ea1e27b37fc53
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-03-23 00:38:20 +0530

    Changes to be committed: 	modified:   random-number.js

--- random-number.js
+++ random-number.js
@@ -1,4 +1,4 @@
 let current = Math.round( Math.random() * 20);
-let monthly_current = 200 - Math.round((Math.random() * 4));
+let monthly_current = 200 - Math.round((Math.random() * 2));
 document.getElementById("current-now").innerHTML = current + " kW";
 document.getElementById("current-monthly").innerHTML = monthly_current + " kW";
