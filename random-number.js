a74a566d15bef6593561a23fc8bbe1a50210dbaf
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-03-23 00:33:08 +0530

    Changes to be committed: 	modified:   dashboard.html 	new file:   random-number.js

---
+++ random-number.js
@@ -0,0 +1,4 @@
+let current = Math.round( Math.random() * 20);
+let monthly_current = 200 - Math.round((Math.random() * 4));
+document.getElementById("current-now").innerHTML = current + " kW";
+document.getElementById("current-monthly").innerHTML = monthly_current + " kW";
