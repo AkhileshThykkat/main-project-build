5e2a84043b873465f96887517ec7cbb7b1fe3c16
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-05-30 00:36:59 +0530

    Changes to be committed: 	modified:   dashboard.html 	deleted:    data-updation.js 	deleted:    kisspng-electricity-computer-icons-symbol-company-electric-electric-5abdc00c68c5a8.0630823215223849084292.jpg 	new file:   logo-color.png

--- data-updation.js
+++
@@ -1,9 +0,0 @@
-let current = (25 + Math.random()*1.75) ;
-var monthly_current = 200 +(Math.random() * 0.25);
-
-document.getElementById('temporary').innerHTML = 'Voltage';
-document.getElementById("current-now").innerHTML = current.toFixed(2) + " V";
-document.getElementById("current-monthly").innerHTML = monthly_current.toFixed(2) + " kW";
-setTimeout(() => {
-    document.location.reload();
-  }, 5500);
